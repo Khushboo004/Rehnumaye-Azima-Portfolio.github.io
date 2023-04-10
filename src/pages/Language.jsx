a619958ca76909679f7704772f75bed8530ef89c
Rehnumaye Khushboo <106239204+Khushboo004@users.noreply.github.com>
2023-04-10 13:06:29 +0600

    sev

--- src/pages/Language.jsx
+++ src/pages/Language.jsx
@@ -17,19 +17,19 @@ const Language = () => {
 <Grid item md ={4} xs={12}>
   <Box p={4} bgcolor="lightgray" borderRadius={4} textAlign="center">
     <h1 className="text-3xl font-bold">English</h1>
-    <p>I can read, write, speak English.</p> {/* Added paragraph text */}
+    {/* <p>I can read, write, speak English.</p>  */}
   </Box>
 </Grid>
 <Grid item md ={4} xs={12}>
   <Box p={4} bgcolor="lightgray" borderRadius={4} textAlign="center">
     <h1 className="text-3xl font-bold">Bangla</h1>
-    <p>I can read, write, speak  Bangla.</p> {/* Added paragraph text */}
+    {/* <p>I can read, write, speak  Bangla.</p>  */}
   </Box>
 </Grid>
 <Grid item md ={4} xs={12}>
   <Box p={4} bgcolor="lightgray" borderRadius={4} textAlign="center">
     <h1 className="text-3xl font-bold">Hindi</h1>
-    <p>I can understand & speak Hindi</p> {/* Added paragraph text */}
+    {/* <p>I can understand & speak Hindi</p>  */}
   </Box>
 </Grid>
 </Grid>
